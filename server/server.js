01ae70c58426c9cecf86e023c3256502d34e68a6
David-Hang-12138 <david.hang.12138@gmail.com>
2023-05-18 18:42:17 +0200

    Moved client into server and get ready for deployment.

--- server/server.js
+++ server/server.js
@@ -21,10 +21,17 @@ app.use(cookieParser());
 app.use("/api/v1", require("./routes/api/v1"));
 app.use(express.static("public"));
 
-// start the Express server
-const INTERNAL_PORT = 5000;
-app.listen(INTERNAL_PORT, () =>
-  logger.info(`RESTful API server started on port ${INTERNAL_PORT}`)
-);
+// serve up production assets
+app.use(express.static("client/build"));
+// let the react app to handle any unknown routes
+// serve up the index.html if express does'nt recognize the route
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+// if not in production use the port 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => logger.info(`Server started on port ${PORT}`));
 
 module.exports = app;
